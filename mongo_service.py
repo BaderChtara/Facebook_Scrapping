99a111c11b21f2d2377e083b35f15eacff730d48
BaderChtara <bader.chtara@esprit.tn>
2021-10-26 11:48:36 +0100

    mongo_service updated

--- mongo_service.py
+++ mongo_service.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://mongodb:27017/")
+client = MongoClient("mongodb://mongo:27017/")
 db = client["Facebook_scrapper"]
 collection = db["posts"]
 
